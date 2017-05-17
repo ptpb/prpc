7a02eeeda04d27fc2a475bf23fcc8234fede99dd
Zack Buhman <zack@buhman.org>
2017-05-17 00:06:16 -0500

    tests: add initial server tests

---
+++ tests/test_server.py
@@ -0,0 +1,71 @@
+from unittest import mock
+from unittest.mock import patch, sentinel
+
+import pytest
+
+from prpc.protocol import base
+from prpc.server import ProtocolServer
+
+
+@pytest.fixture
+def buf():
+    return []
+
+
+@pytest.fixture
+def transport(buf):
+    transport = mock.Mock()
+
+    def write(chunk):
+        buf.append(chunk)
+
+    transport.write.side_effect = write
+
+    return transport
+
+
+@pytest.fixture
+@patch.multiple(base.BaseProtocol, __abstractmethods__=set())
+def server(transport):
+    application = mock.Mock()
+    application.handle_method.return_value = sentinel.return_value
+
+    protocol = base.BaseProtocol
+    server = ProtocolServer(application, protocol)
+
+    server.connection_made(transport)
+
+    return server
+
+
+def test_handle_message_request(buf, server):
+
+    message = base.Request(
+        method=sentinel.method,
+    )
+
+    server.data_received(message)
+
+    server.application.handle_method.assert_called_once_with(
+        sentinel.method
+    )
+
+    assert len(buf) == 1
+    assert buf[0].result == sentinel.return_value
+    assert buf[0].error is None
+    assert buf[0].id == message.id
+
+
+def test_handle_message_notification(buf, server):
+
+    message = base.Notification(
+        method=sentinel.method
+    )
+
+    server.data_received(message)
+
+    server.application.handle_method.assert_called_once_with(
+        sentinel.method
+    )
+
+    assert len(buf) == 0
